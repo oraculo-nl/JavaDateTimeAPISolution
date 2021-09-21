69fc89346d3b37e339dc6536b25c50c9e261b1d0
oraculo-nl <info@oraculo.nl>
2021-09-21 21:59:48 +0200

    first commit

---
+++ src/solution/datetimeapi/Exercise1.java
@@ -0,0 +1,20 @@
+package solution.datetimeapi;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Random;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        var instant = Instant.ofEpochSecond(new Random().nextInt(1_000_000_000));
+        var randomDate = LocalDate.ofInstant(instant, ZoneId.systemDefault());
+        System.out.println(randomDate);
+        //Given the random date, find the date of the previous Thursday
+        System.out.printf("The previous Thursday is: %s%n",
+                randomDate.with(TemporalAdjusters.previous(DayOfWeek.THURSDAY)));
+    }
+}
---
+++ src/solution/datetimeapi/Exercise2.java
@@ -0,0 +1,16 @@
+package solution.datetimeapi;
+
+import java.time.Month;
+import java.time.YearMonth;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        //given an arbitrary year, print out the names of the months and the lengths of the months.
+        var year = 2001;
+        for (Month month : Month.values()) {
+            YearMonth ym = YearMonth.of(year, month);
+            System.out.printf("%s: %d days%n", month, ym.lengthOfMonth());
+        }
+    }
+}
